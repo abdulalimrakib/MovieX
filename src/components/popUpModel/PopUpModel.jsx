4fd8d136d584a0f19acd4de520cfe3d91ec1a00f
abdulalimrakib <abdulalimrakib04@gmail.com>
2024-01-06 13:45:29 +0600

    responsive done

--- src/components/popUpModel/PopUpModel.jsx
+++ src/components/popUpModel/PopUpModel.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import ReactPlayer from "react-player"
+import "./style.scss"
 
 const PopUpModel = ({ show, close, videoData }) => {
 
@@ -14,10 +15,12 @@ const PopUpModel = ({ show, close, videoData }) => {
     } else
         return (
             <div id="close" className="fixed inset-0 z-10 bg-opacity-70 bg-gray-800 flex justify-center items-center" onClick={closeHandle}>
-                <div className="p-5 absolute">
+                <div className="md:p-5 absolute w-full md:w-[640px] md:h-[360px]">
                     <ReactPlayer
-                    url={`https://www.youtube.com/watch?v=${videoData}`}
-                    controls
+                        url={`https://www.youtube.com/watch?v=${videoData}`}
+                        controls
+                        width="100%"
+                        height="100%"
                     />
                 </div>
             </div>
